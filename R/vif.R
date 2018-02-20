05288dc51b3461191c5d9b86bc26138a231da8cc
stp4 <w.peter@statistik-peter.at>
2018-02-20 09:06:36 +0100

    APA_vif ergänzt

--- R/vif.R
+++ R/vif.R
@@ -1,51 +1,73 @@
-#' @rdname VIF
-#' @title VIF Funktion
-#' @name VIF
-#' @description  variance inflation factor, aka VIF  values over 5 are troubling.
-#' should probably investigate anything over 2.5.
-#' Quelle: https://hlplab.wordpress.com/2011/02/24/diagnosing-collinearity-in-lme4/
-#' Kopie der \link{car::vif} funktion
+#' variance inflation factor
+#' 
+#'  VIF  values over 5 are troubling, should probably investigate anything over 2.5.
+#'  
 #' @author Wolfgang Peter
 #' @export
+#' @name VIF
 #' @examples
-#'
-#' set.seed(0815)
-#' fit<-lm(prestige ~ income + education, data=Duncan)
-#' car::vif(fit)
-#' VIF(fit)
-
-
-
-
-
-VIF <- function(x, ...){UseMethod("VIF")}
-
-#' @rdname VIF
-#' @export
-VIF.default <- function(fit, ...) {
-  #print("car")
- round( car::vif(fit), 2)
-  }
-
-#' @rdname VIF
-#' @export
-VIF2 <- function(..., caption = "VIF") {
-  fits <- list(...)
-  for (x in fits) {
-    res <- data.frame(VIF(x, ...))
-    names(res) <- "VIF"
-
-    Output(cbind(Source = rownames(res), res),
-           caption = paste(caption, model_info(x)$y))
-  }
+#' 
+#' \dontrun{
+#'  fit<-lm(prestige ~ income + education, data=Duncan)
+#'  car::vif(fit)
+#'  VIF(fit)
+#' }
+#' 
+VIF <- function(x, ...) {
+  UseMethod("VIF")
 }
 
 #' @rdname VIF
+#' @description VIF.default lm Methode aus car::vif
+#' @export
+VIF.default <- function(fit, ...) {
+  car::vif(fit)
+}
+
+
+#' @rdname APA_
+#' @description  APA_vif:  variance inflation factor, aka VIF
+#' @export
+APA_vif <- function(...,
+                    caption = "VIF",
+                    notes = "") {
+  fits <- list(...)
+  res <- list()
+  i <- 1
+  for (x in fits) {
+    res <- data.frame(VIF(x, ...))
+    names(res) <- "VIF"
+    res <- prepare_output(
+      cbind(Source = rownames(res), res),
+      caption = paste(caption, model_info(x)$y)
+      ,
+      notes = notes
+    )
+    
+    res[[i]] <- res
+    i <- i + 1
+    Output(res)
+  }
+  invisible(res)
+}
+
+
+#' @rdname VIF
+#' @export
+VIF2 <- function(...,
+                 caption = "VIF",
+                 notes = "")  {
+  APA_vif(..., caption = caption, notes = notes)
+}
+
+
+#' @rdname VIF
+#' @description  VIF.merModLmerTest Quelle https://hlplab.wordpress.com/2011/02/24/diagnosing-collinearity-in-lme4/
 #' @export
 VIF.merModLmerTest <- function(fit, ...) {
   #print("VIF.merModLmerTest")
- round( vif.mer(fit),2)
-  }
+  round(vif.mer(fit), 2)
+}
 # VIF.lmerTest <- function(fit, ...)  {
 #   print("VIF.lmerTest")
 #   vif.mer(fit)
@@ -60,37 +82,38 @@ VIF.merModLmerTest <- function(fit, ...) {
 
 vif.mer <- function (fit) {
   ## adapted from rms::vif
-
+  
   v <- stats::vcov(fit)
   nam <- names(lme4::fixef(fit))
-
+  
   ## exclude intercepts
   ns <- sum(1 * (nam == "Intercept" | nam == "(Intercept)"))
   if (ns > 0) {
-    v <- v[-(1:ns), -(1:ns), drop = FALSE]
+    v <- v[-(1:ns),-(1:ns), drop = FALSE]
     nam <- nam[-(1:ns)]
   }
-
-  d <- diag(v)^0.5
-  v <- diag(solve(v/(d %o% d)))
+  
+  d <- diag(v) ^ 0.5
+  v <- diag(solve(v / (d %o% d)))
   names(v) <- nam
   v
 }
 
 kappa.mer <- function (fit,
-                       scale = TRUE, center = FALSE,
+                       scale = TRUE,
+                       center = FALSE,
                        add.intercept = TRUE,
                        exact = FALSE) {
   X <- fit@pp$X
   nam <- names(lme4::fixef(fit))
-
+  
   ## exclude intercepts
   nrp <- sum(1 * (nam == "(Intercept)"))
   if (nrp > 0) {
-    X <- X[, -(1:nrp), drop = FALSE]
+    X <- X[,-(1:nrp), drop = FALSE]
     nam <- nam[-(1:nrp)]
   }
-
+  
   if (add.intercept) {
     X <- cbind(rep(1), scale(X, scale = scale, center = center))
     kappa(X, exact = exact)
@@ -100,7 +123,8 @@ kappa.mer <- function (fit,
 }
 
 colldiag.mer <- function (fit,
-                          scale = TRUE, center = FALSE,
+                          scale = TRUE,
+                          center = FALSE,
                           add.intercept = TRUE) {
   ## adapted from perturb::colldiag, method in Belsley, Kuh, and
   ## Welsch (1980).  look for a high condition index (> 30) with
@@ -120,21 +144,21 @@ colldiag.mer <- function (fit,
       add.intercept <- FALSE
     }
   }
-  X <- X[!is.na(apply(X, 1, all)), ]
-
+  X <- X[!is.na(apply(X, 1, all)),]
+  
   if (add.intercept) {
     X <- cbind(1, X)
     colnames(X)[1] <- "(Intercept)"
   }
   X <- scale(X, scale = scale, center = center)
-
+  
   svdX <- svd(X)
   svdX$d
-  condindx <- max(svdX$d)/svdX$d
+  condindx <- max(svdX$d) / svdX$d
   dim(condindx) <- c(length(condindx), 1)
-
-  Phi = svdX$v %*% diag(1/svdX$d)
-  Phi <- t(Phi^2)
+  
+  Phi = svdX$v %*% diag(1 / svdX$d)
+  Phi <- t(Phi ^ 2)
   pi <- prop.table(Phi, 2)
   colnames(condindx) <- "cond.index"
   if (!is.null(nms)) {
@@ -146,7 +170,7 @@ colldiag.mer <- function (fit,
     colnames(pi) <- 1:ncol(pi)
     rownames(pi) <- 1:nrow(pi)
   }
-
+  
   result <- data.frame(cbind(condindx, pi))
   zapsmall(result)
 }
@@ -156,11 +180,11 @@ maxcorr.mer <- function (fit,
   so <- summary(fit)
   corF <- so@vcov@factors$correlation
   nam <- names(lme4::fixef(fit))
-
+  
   ## exclude intercepts
   ns <- sum(1 * (nam == "Intercept" | nam == "(Intercept)"))
   if (ns > 0 & exclude.intercept) {
-    corF <- corF[-(1:ns), -(1:ns), drop = FALSE]
+    corF <- corF[-(1:ns),-(1:ns), drop = FALSE]
     nam <- nam[-(1:ns)]
   }
   corF[!lower.tri(corF)] <- 0
